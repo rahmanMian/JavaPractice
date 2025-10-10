f9630b024fda62a407c5146f488a3812f6b8dbb2
rahmanMian <rahmanakbarmian@gmail.com>
2025-10-10 18:56:59 -0400

    thinking about it

--- ticTacToe/TicTacToe.java
+++ ticTacToe/TicTacToe.java
@@ -1,1 +1,8 @@
+//whats the move
+// first thought would be to make a arraylisy and store chars in them
+// 0 1 2
+// 3 4 5
+// 6 7 8
+// What are some winnig algos: naah to complicated too many options
+// why am i thinking greedy or some iterative approacat
 
